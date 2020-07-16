3392c80ef74b55b34ab679181bfb4db22035c589
Christian - Darker97 <christian@baltzer.de>
2020-07-16 09:12:58 +0200

    Neubau der Funktionen

--- DarknetFunction.py
+++ DarknetFunction.py
@@ -1,190 +1,20 @@
+
+import argparse
+import time
 import cv2
-import numpy as np
-import json
+import os
 
+Path_cfg  = "Data/yolov3_custom.cfg"
+Path_weight = "Data/yolov3.weights"
+Path_classes = "Data/class.names"
+confidence = 0.5
 
-# -------------------------------------
-# Yolo1
-net = cv2.dnn.readNet("Data/yolov3.weights", "Data/yolov3_custom.cfg")
+def Auswertung_Yolo(Path_image):
+    pass
 
-def Yolo1(LinkToFile):
-    layer_names = net.getLayerNames()
-    outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+def CutTable(image, box):
+    pass
 
-    #loading image
-    img = cv2.imread(LinkToFile)
-    img = cv2.resize(img,None,fx=0.4,fy=0.3)
-    height,width,channels = img.shape
+def Paint_Yolo(image, box):
+    pass
 
-
-    blob = cv2.dnn.blobFromImage(img,0.00392,(416,416),(0,0,0),True,crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(outputlayers)
-
-    class_ids=[]
-    confidences=[]
-    boxes=[]
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = int(np.argmax(scores))
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                #onject detected
-                center_x= int(detection[0]*width)
-                center_y= int(detection[1]*height)
-                w = int(detection[2]*width)
-                h = int(detection[3]*height)
-            
-                #cv2.circle(img,(center_x,center_y),10,(0,255,0),2)
-                #rectangle co-ordinaters
-                x=int(center_x - w/2)
-                y=int(center_y - h/2)
-                #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                
-                boxes.append([x,y,w,h]) #put all rectangle areas
-                confidences.append(float(confidence)) #how confidence was that object detected and show that percentage
-                class_ids.append(class_id) #name of the object tha was detected
-
-    indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.6)
-
-    Temp_Array = []
-
-    for i in range(len(boxes)-1):
-        temp = {}
-        temp['box'] = boxes[i]
-        temp['confidece'] = confidences[i]
-        temp['class'] = class_ids[i]
-        Temp_Array.append(temp)
-
-    print(Temp_Array)
-    Temp_Array = json.dumps(Temp_Array)
-
-    return Temp_Array
-
-
-# -------------------------------------
-# Document Detection
-
-def DocumentDetection(LinkToFile):
-    img = cv2.imread(LinkToFile)
-    
-    FindeDocument(img)
-
-    Aufbereitung(img)
-
-    MachDokumentSchick(img)
-
-    return img
-
-def FindeDocument(image):
-
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    img = cv2.bilateralFilter(img, 9, 75, 75)
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 4)
-
-    img = cv2.medianBlur(img, 11)
-    
-    img = cv2.copyMakeBorder(img, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-    edges = cv2.Canny(img, 200, 250)
-
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Finding contour of biggest rectangle
-    # Otherwise return corners of original image
-    # Don't forget on our 5px border!
-    height = edges.shape[0]
-    width = edges.shape[1]
-    MAX_COUNTOUR_AREA = (width - 10) * (height - 10)
-
-    # Page fill at least half of image, then saving max area found
-    maxAreaFound = MAX_COUNTOUR_AREA * 0.5
-
-    # Saving page contour
-    pageContour = np.array([[5, 5], [5, height-5], [width-5, height-5], [width-5, 5]])
-
-    # Go through all contours
-    for cnt in contours:
-        # Simplify contour
-        perimeter = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, 0.03 * perimeter, True)
-
-        # Page has 4 corners and it is convex
-        # Page area must be bigger than maxAreaFound 
-        if (len(approx) == 4 and
-                cv2.isContourConvex(approx) and
-                maxAreaFound < cv2.contourArea(approx) < MAX_COUNTOUR_AREA):
-
-            maxAreaFound = cv2.contourArea(approx)
-            pageContour = approx
-
-    # Result in pageConoutr (numpy array of 4 points):
-
-    def fourCornersSort(pts):
-        """ Sort corners: top-left, bot-left, bot-right, top-right """
-        # Difference and sum of x and y value
-        # Inspired by http://www.pyimagesearch.com
-        diff = np.diff(pts, axis=1)
-        summ = pts.sum(axis=1)
-        
-        # Top-left point has smallest sum...
-        # np.argmin() returns INDEX of min
-        return np.array([pts[np.argmin(summ)],
-                        pts[np.argmax(diff)],
-                        pts[np.argmax(summ)],
-                        pts[np.argmin(diff)]])
-
-
-    def contourOffset(cnt, offset):
-        """ Offset contour, by 5px border """
-        # Matrix addition
-        cnt += offset
-        
-        # if value < 0 => replace it by 0
-        cnt[cnt < 0] = 0
-        return cnt
-
-
-    # Sort and offset corners
-    pageContour = fourCornersSort(pageContour[:, 0])
-    pageContour = contourOffset(pageContour, (-5, -5))
-
-    # Recalculate to original scale - start Points
-    sPoints = pageContour.dot(image.shape[0] / 800)
-    
-    # Using Euclidean distance
-    # Calculate maximum height (maximal length of vertical edges) and width
-    height = max(np.linalg.norm(sPoints[0] - sPoints[1]),
-                np.linalg.norm(sPoints[2] - sPoints[3]))
-    width = max(np.linalg.norm(sPoints[1] - sPoints[2]),
-                np.linalg.norm(sPoints[3] - sPoints[0]))
-
-    # Create target points
-    tPoints = np.array([[0, 0],
-                        [0, height],
-                        [width, height],
-                        [width, 0]], np.float32)
-
-    # getPerspectiveTransform() needs float32
-    if sPoints.dtype != np.float32:
-        sPoints = sPoints.astype(np.float32)
-
-    # Wraping perspective
-    M = cv2.getPerspectiveTransform(sPoints, tPoints) 
-    newImage = cv2.warpPerspective(image, M, (int(width), int(height)))
-
-    cv2.imwrite("resultImage.jpg", cv2.cvtColor(newImage, cv2.COLOR_BGR2RGB))
-    
-    return "resultImage.jpg"
-
-def MachDokumentSchick(img):
-
-
-    return img
-
-def Aufbereitung(img):
-    
-    
-    return img
